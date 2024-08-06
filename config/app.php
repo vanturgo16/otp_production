d522da1773062343ea632d5cdf9198995be20c89
rindanwijaya <rindan.wijaya@gmail.com>
2024-08-06 11:06:04 +0700

    Delete yajra config

--- config/app.php
+++ config/app.php
@@ -169,7 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 		
-		Yajra\DataTables\DataTablesServiceProvider::class,
+		//Yajra\DataTables\DataTablesServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -185,7 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-		'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+		//'DataTables' => Yajra\DataTables\Facades\DataTables::class,
     ])->toArray(),
 
 ];
